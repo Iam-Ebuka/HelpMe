4d0379833bc93f2acbd80e3f790a8bd3144785db
Ebuka Odiah <ebukagrizz@gmail.com>
2023-10-02 23:43:41 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,16 @@ function Header() {
     <div>
         <div className='Navbar'>
             <div className='NavbarCushon'>
+               <Link to={"../ho"} style={{textDecoration:"none", color:"inherit"}}>
                 <img src={hoome} alt='logo' className='hoome'/>
+                 </Link>
                 <div className='middle'>
                     <Link to={"../ready"} style={{textDecoration:"none", color:"inherit"}}>
                     <p>Features</p>
                     </Link>
+                  <Link to={"../"} style={{textDecoration:"none", color:"inherit"}}>
                     <p>How It Works</p>
-                    
+                    </Link>
                 </div>
                 <Link to={'../signin'} style={{textDecoration:'none',color:'inherit'}}>
                 <div className='end'>
